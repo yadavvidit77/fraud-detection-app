5380fe9661e3ba3835557ee1849c0ab1b56928e1
yadavvidit77 <yadavvidit77@gmail.com>
2026-04-01 22:07:59 +0530

    app.py

--- app.py
+++ app.py
@@ -363,6 +363,6 @@ def health():
  
 # ── Start server ─────────────────────────────────────────────
 if __name__ == '__main__':
-    print("\n  FraudGuard running → http://localhost:5001\n")
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    print("\n  FraudGuard running → http://localhost:5000\n")
+    app.run(debug=True, host='0.0.0.0', port=5000)
  
